f7c3430e229f777c203d1ca73d7eb3b08e846166
kerrybli <kerry.blig12@gmail.com>
2022-02-28 15:26:12 +0530

    completed the assignment

--- src/app/directive/directive.component.ts
+++ src/app/directive/directive.component.ts
@@ -3,5 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-directive',
   templateUrl: './directive.component.html',
+  styleUrls: ['./directive.component.css'],
 })
-export class DirectiveComponent {}
+export class DirectiveComponent {
+  passage: string = 'Hello Button';
+  buttonStatus: boolean = false;
+  buttonUpdatedLog: any = [];
+
+  buttonToggle() {
+    this.buttonStatus = !this.buttonStatus;
+    this.buttonUpdatedLog.push(this.buttonUpdatedLog.length + 1);
+  }
+  updateParagraph() {
+    this.buttonStatus = true;
+    this.buttonUpdatedLog.push(this.passage);
+    console.log('this.passage:', this.passage);
+  }
+}
